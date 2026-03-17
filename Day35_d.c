69250c27c9ce70c2f3b02ff9ce06003427de7da9
YugrajaSamadhiya <yugrajasam@gmail.com>
2026-03-17 12:43:19 +0530

    DAY 35

--- Day35_d.c
+++ Day35_d.c
@@ -19,3 +19,39 @@ Output:
 Explanation:
 Use array and front/rear pointers. Enqueue inserts at rear, dequeue removes from front.
  Display from front to rear.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+void enqueue(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+    if (rear == NULL) {
+        front = rear = temp;
+    } else {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+    struct Node* temp = front;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    return 0;
+}
