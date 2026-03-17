d6bee90de4de982bbc846057291e216c9aec9622
YugrajaSamadhiya <yugrajasam@gmail.com>
2026-03-17 12:56:05 +0530

    DAY 45

--- Day45_d.c
+++ Day45_d.c
@@ -16,3 +16,55 @@ Input:
 Output:
 3
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+int height(struct Node* root) {
+    if (root == NULL) return 0;
+    int l = height(root->left);
+    int r = height(root->right);
+    return (l > r ? l : r) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* nodes[1000] = {NULL};
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = newNode(arr[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n) nodes[i]->left = nodes[left];
+            if (right < n) nodes[i]->right = nodes[right];
+        }
+    }
+
+    printf("%d", height(nodes[0]));
+    return 0;
+}
