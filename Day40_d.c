86a2b1972b6a03c2b4d993812ea1c7d8b9a05077
YugrajaSamadhiya <yugrajasam@gmail.com>
2026-03-17 12:50:02 +0530

    DAY 40

--- Day40_d.c
+++ Day40_d.c
@@ -1,4 +1,50 @@
 /*Implement Heap Sort using a Max Heap to sort an array in ascending order. 
 First build a max heap, then repeatedly extract the maximum element and place it at
  the end of the array.*/
- 
+ #include <stdio.h>
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    while (1) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(&arr[i], &arr[largest]);
+            i = largest;
+        } else break;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
