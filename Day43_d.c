08204557f714afc593b60cd1eba383a1adc15a4e
YugrajaSamadhiya <yugrajasam@gmail.com>
2026-03-17 12:52:59 +0530

    DAY 43

--- Day43_d.c
+++ Day43_d.c
@@ -15,3 +15,56 @@ Input:
 
 Output:
 4 2 5 1 3 6*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (n == 0 || arr[0] == -1) return 0;
+
+    struct Node* nodes[1000] = {NULL};
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = newNode(arr[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n) nodes[i]->left = nodes[left];
+            if (right < n) nodes[i]->right = nodes[right];
+        }
+    }
+
+    inorder(nodes[0]);
+    return 0;
+}
