cc2d91bbdebf2888f4981f7902529e8e496929a8
YugrajaSamadhiya <yugrajasam@gmail.com>
2026-03-17 12:55:14 +0530

    DAY 44

--- Day44_d.c
+++ Day44_d.c
@@ -17,3 +17,73 @@ Output:
 4 2 5 1 6 3 7
 1 2 4 5 3 6 7
 4 5 2 6 7 3 1*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[1000];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* nodes[1000] = {NULL};
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1) {
+            nodes[i] = newNode(arr[i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n) nodes[i]->left = nodes[left];
+            if (right < n) nodes[i]->right = nodes[right];
+        }
+    }
+
+    inorder(nodes[0]);
+    printf("\n");
+    preorder(nodes[0]);
+    printf("\n");
+    postorder(nodes[0]);
+
+    return 0;
+}
