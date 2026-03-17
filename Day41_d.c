4375bfd02b7807e4dd6d5b0e82d9d5e582ed0164
YugrajaSamadhiya <yugrajasam@gmail.com>
2026-03-17 12:50:58 +0530

    DAY 41

--- Day41_d.c
+++ Day41_d.c
@@ -8,3 +8,54 @@ Input Format:
 Output Format:
 - Print dequeued elements
 - Print -1 if dequeue is attempted on an empty queue*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+void enqueue(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = NULL;
+    if (rear == NULL) {
+        front = rear = temp;
+    } else {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+int dequeue() {
+    if (front == NULL) return -1;
+    struct Node* temp = front;
+    int val = temp->data;
+    front = front->next;
+    if (front == NULL) rear = NULL;
+    free(temp);
+    return val;
+}
+
+int main() {
+    int n, x;
+    char op[20];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", op);
+        if (strcmp(op, "enqueue") == 0) {
+            scanf("%d", &x);
+            enqueue(x);
+        } else if (strcmp(op, "dequeue") == 0) {
+            printf("%d\n", dequeue());
+        }
+    }
+
+    return 0;
+}
